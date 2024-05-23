ecb658f8467e4c5079894c70b1f31a46729fb40a
mdiab1 <moustapha.diab5@gmail.com>
2024-05-23 12:13:47 -0500

    Final updates with webpage updates

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import MenuItemView, SingleMenuItemView, BookingView, index
+#from .views import MenuItemView, SingleMenuItemView, BookingView, home, about, menu, book
+from .views import MenuItemView, SingleMenuItemView, BookingView, home, about
 
 urlpatterns = [
-    path('menu/items/', MenuItemView.as_view()),
-    path('menu/items/<int:pk>', SingleMenuItemView.as_view()),
-    path('booking/tables', BookingView.as_view()),
-    path('', index, name='home')
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    #path('book/', book, name="book"),
+    #path('menu/', menu, name="menu"),
+    path('menu-items/', MenuItemView.as_view()),
+    path('menu-items/<int:pk>', SingleMenuItemView.as_view()),
+    path('bookings/', BookingView.as_view()),
 ]
